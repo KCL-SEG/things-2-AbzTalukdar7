685380a65983bdc3db59958e8882a2cbead9d281
Abed Talukdar <talukdar.a@hotmail.com>
2022-10-24 00:09:55 +0100

    add thing form

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,14 @@
 """Forms of the project."""
+from django import forms
+from .models import Thing
+from django.core.validators import RegexValidator
 
-# Create your forms here.
+class ThingForm(forms.ModelForm):
+    class Meta:
+        model = Thing
+        fields = ['name', 'description']
+        widgets = {'description': forms.Textarea()}
+
+    quantity = forms.IntegerField(
+        label='quantity',
+        widget = forms.NumberInput()
